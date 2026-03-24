d1c8e8ec642c5c40be80cbe2832b8ffe50fb7d6a
Valerii Brykalov <brival1005@gmail.com>
2026-03-24 02:25:31 +0100

    fix status code

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -25,7 +25,7 @@ export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findByIdAndDelete(noteId);
   if (!note) {
-    throw createHttpError('404', 'Note not found');
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
@@ -38,7 +38,7 @@ export const updateNote = async (req, res) => {
   });
 
   if (!note) {
-    throw createHttpError('404', 'Note not found');
+    throw createHttpError(404, 'Note not found');
   }
   res.status(200).json(note);
 };
